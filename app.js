91115f815254c6395d07c6cf28f99e6cb37f440d
Maxim Sinelnikov <maxim@sinelnikov.info>
2019-03-10 18:20:32 +0100

    cloud mongoDB connection

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+require("dotenv").config();
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${
+    process.env.DB_PASS
+  }@cluster0-ys2zg.mongodb.net/test?retryWrites=true`
+);
+
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
 
 app.use("/graphql", graphqlHTTP({ schema: schema, graphiql: true }));
 
